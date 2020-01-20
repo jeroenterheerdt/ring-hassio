c81f4a8351030a3ee9ffc6aa8cc1d5610df7a81b
jeroenterheerdt <jeroen_ter_heerdt@hotmail.com>
2020-01-19 19:25:56 -0800

    Updating Dockerfile

--- ring_hassio/run.sh
+++ ring_hassio/run.sh
@@ -1,1 +1,6 @@
 echo Hello World!
+CONFIG_PATH=/data/options.json
+export RING_EMAIL="$(jq --raw-output '.ring_username' $CONFIG_PATH)"
+export RING_PASS="$(jq --raw-output '.ring_password' $CONFIG_PATH)"
+
+node livestream.js
