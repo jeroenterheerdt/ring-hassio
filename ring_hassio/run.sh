0169c9f5ed04c042ecacfce18939972271b355cf
Jeroen ter Heerdt <jeroen_ter_heerdt@hotmail.com>
2020-01-23 08:39:07 -0800

    Update run.sh

--- ring_hassio/run.sh
+++ ring_hassio/run.sh
@@ -2,5 +2,5 @@ echo Hello World!
 CONFIG_PATH=/data/options.json
 export RING_EMAIL="$(jq --raw-output '.ring_username' $CONFIG_PATH)"
 export RING_PASS="$(jq --raw-output '.ring_password' $CONFIG_PATH)"
-
+cd /ring-hassio/ring_hassio
 node livestream.js
