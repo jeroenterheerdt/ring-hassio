72fd0e49a50468e25fc12c93d0b5f37e03056833
Jeroen ter Heerdt <jterh@microsoft.com>
2020-01-23 16:16:36 -0800

    updating run.sh

--- ring_hassio/run.sh
+++ ring_hassio/run.sh
@@ -1,8 +1,8 @@
 echo Hello World!
 CONFIG_PATH=/data/options.json
-cat $CONFIG_PATH
 export RING_EMAIL="$(jq --raw-output '.ring_username' $CONFIG_PATH)"
 export RING_PASS="$(jq --raw-output '.ring_password' $CONFIG_PATH)"
-export PORT="$(jq --raw-output '.port' $CONFIG_PATH)"
+export PORT="$(bashio::addon.port xy)"
+echo $PORT
 cd /ring-hassio/ring_hassio
 node livestream.js
